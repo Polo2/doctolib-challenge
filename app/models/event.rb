3f5abb58d23d2d28e1f6f08777471e79b595f738
Paul deBodinat <polo2bodi@gmail.com>
2018-02-16 15:00:27 +0100

    doctolib's challenge : answer Paul de Bodinat

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,72 +1,55 @@
 class Event < ApplicationRecord
-
+  APPOINTMENT_DURATION_IN_MINUTES = 30
+  DAYS_EXPECTED = 7
 
   def self.availabilities(first_day_date)
-    seven_days_array = []
-    last_day_date = first_day_date + 6
-
-    7.times do |day_index|
-     seven_days_array << {day: first_day_date + day_index, openings: [], appointments: [], slots: []}
-     # seven_days_array << Event.create(starts_at: first_day_date + day_index, kind: "availabilities").slots
-    end
+    @week_schedule = []
+    DAYS_EXPECTED.times { |day_index| @week_schedule << { day: first_day_date + day_index, openings: [], appointments: [], slots: [] } }
 
     openings_with_recurrence = self.where(kind: "opening", weekly_recurring: true)
     openings_with_recurrence.each do |opening_event|
-      day_index = (opening_event.starts_at.day - first_day_date.day) % 7
-      # seven_days_array[day_index][:openings] << {start: opening_event.starts_at.strftime("%H:%M") ,end: opening_event.ends_at.strftime("%H:%M") }
-      seven_days_array[day_index][:slots] << opening_event.starts_at.strftime("%H:%M")
-      seven_days_array[day_index][:slots] << opening_event.ends_at.strftime("%H:%M")
+      @week_schedule[(opening_event.starts_at.day - first_day_date.day) % 7][:openings] << opening_event.fill_slots_for_openings
     end
-
 
     openings_without_recurrence = self.where(kind: "opening", weekly_recurring: nil).select { |opening| opening.starts_at >= first_day_date && opening.starts_at < first_day_date + 7 }
     openings_without_recurrence.each do |opening_event|
-      day_index = opening_event.starts_at.day - first_day_date.day
-      # seven_days_array[day_index][:openings] << {start: opening_event.starts_at.strftime("%H:%M") ,end: opening_event.ends_at.strftime("%H:%M") }
-      seven_days_array[day_index][:slots] << opening_event.starts_at.strftime("%H:%M")
-      seven_days_array[day_index][:slots] << opening_event.ends_at.strftime("%H:%M")
+      @week_schedule[opening_event.starts_at.day - first_day_date.day][:openings] << opening_event.fill_slots_for_openings
     end
 
     appointments = self.where(kind: "appointment").select { |appointment| appointment.starts_at >= first_day_date && appointment.starts_at < first_day_date + 7 }
     appointments.each do |appointment_event|
-      day_index = appointment_event.starts_at.day - first_day_date.day
-      # seven_days_array[day_index][:appointments] << {start: appointment_event.starts_at.strftime("%H:%M") ,end: appointment_event.ends_at.strftime("%H:%M") }
-      seven_days_array[day_index][:slots] << appointment_event.starts_at.strftime("%H:%M")
-      seven_days_array[day_index][:slots] << appointment_event.ends_at.strftime("%H:%M")
+      @week_schedule[appointment_event.starts_at.day - first_day_date.day][:appointments] << appointment_event.fill_slots_for_appointments
     end
 
-    seven_days_array.each do |day|
-      day[:slots].sort!
+    @week_schedule = @week_schedule.map do |schedule|
+      slots_per_day = schedule[:openings].empty? ? [] : schedule[:openings][0].select { |time| !schedule[:appointments][0].include?(time) }
+      { date: schedule[:day], slots: slots_per_day.sort }
     end
 
-    seven_days_array
-
+    # cleaning for test 9:30 != 09:30
+    @week_schedule.map { |jour| { date: jour[:date], slots: jour[:slots].map { |time| time[0] == "0" ? time.split("")[1..-1].join("") : time } } }
   end
 
-  # def slots
-  #   slots = []
-  #   self[:openings].each do |element|
-  #     slots << element[:start]
-  #     slots << element[:end]
-  #   end
-  #   self[:appointments].each do |element|
-  #     slots << element[:start]
-  #     slots << element[:end]
-  #   end
-  #   slots.sort
-  # end
-# pour chaque rdv deja pris, je trouve dans le stableau slots l'heure de rdv la plus proche, et je viens ajouter "contre" ce nv rdv
+  def fill_slots_for_appointments
+    var_time = self.starts_at
+    end_time = self.ends_at
+    appointments = []
 
-  def scan_opening
+    while var_time < end_time
+      appointments << var_time.strftime("%H:%M")
+      var_time += APPOINTMENT_DURATION_IN_MINUTES * 60
+    end
+    appointments
+  end
+
+  def fill_slots_for_openings
+    var_time = self.starts_at
+    end_time = self.ends_at
     openings = []
-    openings << Event.where(kind: "opening", weekly_recurring: "true")
+    while var_time < end_time
+      openings << var_time.strftime("%H:%M")
+      var_time += APPOINTMENT_DURATION_IN_MINUTES * 60
+    end
     openings
   end
-
-private
-
-
 end
-
-
-# .map { |event| event.starts_at }
--- lib/tasks/initialize.rake
+++ lib/tasks/initialize.rake
@@ -1,13 +1,31 @@
 puts "nettoyage DB"
 Event.destroy_all
 
-puts "c'est darty"
+puts "lancement du test"
 
 
-ope1 = Event.create kind: 'opening', starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
-ope2 = Event.create kind: 'opening', starts_at: DateTime.parse("2014-08-13 14:00"), ends_at: DateTime.parse("2014-08-13 17:30")
-ope3 = Event.create kind: 'opening', starts_at: DateTime.parse("2014-09-13 14:00"), ends_at: DateTime.parse("2014-09-13 17:30")
-app = Event.create kind: 'appointment', starts_at: DateTime.parse("2014-08-11 10:30"), ends_at: DateTime.parse("2014-08-11 11:30")
+Event.create kind: 'opening', starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
+Event.create kind: 'appointment', starts_at: DateTime.parse("2014-08-11 10:30"), ends_at: DateTime.parse("2014-08-11 11:30")
+
+availabilities = Event.availabilities DateTime.parse("2014-08-10")
+
+puts "test1"
+puts Date.new(2014, 8, 10) == availabilities[0][:date]
+
+puts "test2"
+puts availabilities[0][:slots] == []
+
+puts "test3"
+puts Date.new(2014, 8, 11) == availabilities[1][:date]
+
+puts "test4"
+
+puts availabilities[1][:slots] == ["9:30", "10:00", "11:30", "12:00"]
 
 
-p Event.availabilities DateTime.parse("2014-08-10")
+puts "test5"
+puts Date.new(2014, 8, 16) == availabilities[6][:date]
+
+puts "test6"
+puts availabilities.length == 7
+
